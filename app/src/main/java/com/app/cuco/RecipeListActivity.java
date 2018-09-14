003ac16572195edf582f631797d9d65fa041f166
Wesley Belarmino <raaphaeell@gmail.com>
2018-09-14 14:44:36 -0400

    - initial commit

---
+++ app/src/main/java/com/app/cuco/RecipeListActivity.java
@@ -0,0 +1,12 @@
+package com.app.cuco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
